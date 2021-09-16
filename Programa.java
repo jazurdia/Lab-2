70b8cfc3d9b64c9c2136636dd955be06336aad9c
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2021-09-15 20:26:44 -0600

    Create Programa.java
    Programa will be instanced every time a program is directed to the ram in the simulator.

---
+++ Programa.java
@@ -0,0 +1,36 @@
+public class Programa {
+    
+    String nombre;
+    int espacio;
+    int ciclos;
+
+    /**
+     * 
+     * @param nombre : nombre del programa
+     * @param espacio : espacio en la memoria RAM que ocupa el programa
+     * @param ciclos : número de ciclos que toma ejecutar el programa. 
+     */
+    
+    public Programa(String nombre, int espacio, int ciclos){
+        this.nombre = nombre;
+        this.espacio = espacio;
+        this.ciclos = ciclos;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getEspacio(){
+        return espacio;
+    }
+
+    public int getCiclos(){
+        return ciclos;
+    }
+
+    public void exciclos(){
+        ciclos --;
+    }
+
+}
